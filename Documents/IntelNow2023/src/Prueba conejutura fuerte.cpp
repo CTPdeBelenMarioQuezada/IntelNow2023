fdf62719b6db54f082e618a0db50e07e3d648764
Mario Quezada <marioqzada16@gmail.com>
2023-07-29 15:31:54 -0600

    Se agrega nuestro proyecto

---
+++ Documents/IntelNow2023/src/Prueba conejutura fuerte.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+const int N=22222222 // tamano maximo parza que el algoritmo genere una lista de numero primos 
+
+int main(){
+	
+	
+	int Numero, i, ; //  i = el numero por el cual se multiplica Numero.
+	
+	std:: cin >> Numero;
+	 
+	  bool primo [N]; //indica si es primo el numero 
+	  
+	  for (i = 0 ; i < Numero; i++) [
+	primo[i] = true; 
+	]
+	
+	
+	  }
+	  
+	  
+	
+}
