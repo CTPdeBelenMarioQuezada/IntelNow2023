f18a831ed9e9b1ac97a265dc32793b49f7f5250f
CTPdeBelenMarioQuezada <140217116+CTPdeBelenMarioQuezada@users.noreply.github.com>
2023-08-01 14:10:22 -0600

    Prueba2conjeturadebil
    Se intentó realizar un cambio al algoritmo de la conjetura fuerte para ver si se podría ser útil el mismo código, simplemente modificando algunas cosas.
    No funcionó, ya que dio error, se seguirá buscando alguna manera para modificarla y no tener que hacer un código totalmente diferente y que siga la misma logica que el de la conjetura fuerte.

---
+++ Documents/IntelNow2023/src/Pruebaconjeturadebil2.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+bool esPrimo(int numero);
+
+int main()
+{
+    int a, b;
+    do
+    {
+        cout << "Introduzca el valor de a y b: ";
+        cin >> a >> b;
+    } while (a >= b);
+
+    if (a % 2 != 0)
+        a++;
+
+    for (int i = a; i <= b; i += 3)
+    {
+        for (int n1 = 3; n1 <= i / 3; n1++)
+        {
+            int n2 = i - n1 
+			int n3 = n;
+            if (esPrimo(n1) && esPrimo(n2)) && esPrimo((n3));
+                cout << i << " = " << n1 << " + " << n2  "+" << n3<< endl;
+        }
+    }
+}
+
+bool esPrimo(int numero)
+{
+    if (numero < 2)
+        return false;
+
+    for (int i = 3; i <= sqrt(numero); i+= 2)
+    {
+        if (numero % i == 0)
+            return false;
+    }
+
+    return true;
+}
